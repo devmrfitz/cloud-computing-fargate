d42d80d019a359692ac703510cda9fc7b49bdb5a
mishraanis <mishraanis@gmail.com>
2022-12-19 16:45:14 +0530

    debug

--- pages/api/delete.js
+++ pages/api/delete.js
@@ -3,12 +3,12 @@ export default function handler(req, res) {
   const {repoURL, kubeConfigPath} = req.body;
   const folderName = repoURL.split('/').pop().split('.').shift()+ Math.floor(Math.random() * 1000);
   // clone the repo
-  shell.exec(`git clone ${repoURL} /tmp/${folderName}`);
+  shell.exec(`vagrant ssh leader -c "git clone ${repoURL} /tmp/${folderName}"`);
 
   // deploy the repo
-  shell.exec(`kubectl delete -f /tmp/${folderName}/${kubeConfigPath}`);
+  shell.exec(`vagrant ssh leader -c "kubectl delete -f /tmp/${folderName}/${kubeConfigPath}"`);
 
-  shell.rm('-rf', `/tmp/${folderName}`);
+  shell.rm('-rf', `vagrant ssh leader -c "/tmp/${folderName}"`);
 
   res.status(201).json({ success: true })
 }
--- pages/api/get_logs.js
+++ pages/api/get_logs.js
@@ -3,10 +3,10 @@ export default function handler(req, res) {
   const {repoURL1, kubeConfigPath1} = req.body;
   const folderName = repoURL1.split('/').pop().split('.').shift()+ Math.floor(Math.random() * 1000);
   // clone the repo
-  shell.exec(`git clone ${repoURL1} /tmp/${folderName}`);
+  shell.exec(`vagrant ssh leader -c "git clone ${repoURL1} /tmp/${folderName}"`);
   // deploy the repo
-  let output = shell.exec(`kubectl describe -f /tmp/${folderName}/${kubeConfigPath1}`);
+  let output = shell.exec(`vagrant ssh leader -c "kubectl describe -f /tmp/${folderName}/${kubeConfigPath1}"`);
   // let output = shell.exec(`kubectl cluster-info dump`);
-  shell.rm('-rf', `/tmp/${folderName}`);
+  shell.rm('-rf', `vagrant ssh leader -c "/tmp/${folderName}"`);
   res.status(201).json({ success: true, logs: output });
 }
