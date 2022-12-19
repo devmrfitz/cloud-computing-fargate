1c244696efb5207610794c3081edc51743ef26c3
Aditya Pratap Singh <adityapratapsingh51@gmail.com>
2022-12-19 17:04:14 +0530

    deploy and delete done

---
+++ pages/status.jsx
@@ -0,0 +1,22 @@
+import {useEffect, useState} from "react";
+
+export default function Status(req, res) {
+  const [status, setStatus] = useState('loading');
+
+  useEffect(() => {
+    fetch('/api/show_status')
+      .then(res => res.json())
+      .then(data => {
+        setStatus(data.status);
+      })
+  }, []);
+
+
+
+  return (
+    <pre>
+      {status}
+    </pre>
+  )
+
+}
